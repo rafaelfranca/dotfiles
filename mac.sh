247deb59e10fcfc1724563fb746771cf18679a1c
Rafael Mendonça França <rafaelmfranca@gmail.com>
2011-04-07 18:28:45 -0300

    Using MacVim

--- mac.sh
+++ mac.sh
@@ -3,11 +3,12 @@ if [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
 fi
 
-EDITOR="vim -p"
+EDITOR="mvim -v"
 export EDITOR="$EDITOR"
 
 alias vi="$EDITOR"
 alias vim="$EDITOR"
+alias vimdiff="mvimdiff -v"
 
 alias ls="ls -G"
 alias gg="launch gitx"
