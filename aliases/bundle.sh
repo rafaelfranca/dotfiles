e5aa977ae2bd6f9c8107922982fe665ef9753570
Rafael Mendonça França <rafaelmfranca@gmail.com>
2011-09-03 22:31:02 -0300

    Now using binstubs for bundle

--- aliases/bundle.sh
+++ aliases/bundle.sh
@@ -1,5 +1,5 @@
 alias b="bundle"
-alias bi="b install --path vendor"
+alias bi="b install --binstubs=b --path vendor"
 alias bu="b update"
 alias be="b exec"
 alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
