a39aab1c16f40b2ecadc2dec3dbaf7b025377ccb
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2011-12-16 14:11:25 -0200

    Remove comments

--- dotfiles/js/github.com.js
+++ dotfiles/js/github.com.js
@@ -1,19 +1,4 @@
-// ==UserScript==
-// @name                GitHub delete all notifications button
-// @namespace	        http://www.github.com/micolous/delete-all-notifications
-// @description	        Adds a button to allow you to delete all notifications on a page.
-// @include		http://github.com/inbox/notifications*
-// @include		https://github.com/inbox/notifications*
-// ==/UserScript==
-/*
-
-Sick of hipster jerks posting in commit or issue threads, and you made the
-mistake of contributing something useful in it?  This userscript (also Chrome
-compatible) gives you a handy button to delete all of the notifications on a
-page.  Just visit the notifications page, and this will give you a button to
-handily delete their crap.
-
-*/
+// Need http://defunkt.io/dotjs/ to work
 
 (function() {
   var $button = $('<a><span>Delete all notifications</span></a>').addClass('minibutton').
