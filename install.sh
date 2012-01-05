c7b2e41249a13780d359e696b38c50e8213ecf96
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2012-01-05 12:14:45 -0300

    Change install script to install my vimfiles

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ for i in ~/.dotfiles/dotfiles/*; do
   rm -rf ~/.$(basename $i)
   ln -s $i ~/.$(basename $i)
 done
+
+if [[ ! -d "$HOME/.vim" ]]; then
+  echo "Installing vimfiles"
+  git clone git://github.com/rafaelfranca/vimfiles.git $HOME/.vim
+  cd $HOME/.vim && git submodule update --init
+fi
