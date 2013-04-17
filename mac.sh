cd84c5f813a9a3402c509f903414620224218591
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-04-17 16:19:24 -0300

    Add some alias to not scare people not using Boxen

--- mac.sh
+++ mac.sh
@@ -5,6 +5,9 @@ fi
 
 EDITOR="mvim -v"
 export EDITOR="$EDITOR"
+export REDIS_URL="$BOXEN_REDIS_URL"
+export REDIS_PORT="$BOXEN_REDIS_PORT"
+export MONGODB_PORT="$BOXEN_MONGODB_PORT"
 
 alias vi="$EDITOR"
 alias vim="$EDITOR"
