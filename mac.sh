c8fa3ab381bb5994e3bc43101eac0a38f61ed0a2
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-04-17 16:18:46 -0300

    No need to load git-prompt since it is loaded by bash_completion now

--- mac.sh
+++ mac.sh
@@ -3,10 +3,6 @@ if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
 
-if [ -f $(brew --prefix)/share/git-core/contrib/completion/git-prompt.sh ]; then
-  . $(brew --prefix)/share/git-core/contrib/completion/git-prompt.sh
-fi
-
 EDITOR="mvim -v"
 export EDITOR="$EDITOR"
 
