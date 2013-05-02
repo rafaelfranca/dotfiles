73428fbcf2eec9c06ed543dead58b2a497a6aa4d
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-05-02 15:22:52 -0300

    bi is only bundle install now.
    I'm not using binstubs and path anymore.
    
    Also --binstubs has problem with Rails 4

--- aliases/bundle.sh
+++ aliases/bundle.sh
@@ -1,5 +1,4 @@
 alias b="bundle"
-alias bi="b install --binstubs=b --path vendor"
+alias bi="b install"
 alias bu="b update"
 alias be="b exec"
-alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
