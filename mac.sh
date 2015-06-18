9c7a120e46f69f90c52718c48132c19c4f5a48b4
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-06-18 20:59:19 -0300

    Remove unneeded config

--- mac.sh
+++ mac.sh
@@ -1,8 +1,3 @@
-# Bash Completion (install bash-completion with homebrew to enable it
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-
 EDITOR="mvim -v"
 export EDITOR="$EDITOR"
 
